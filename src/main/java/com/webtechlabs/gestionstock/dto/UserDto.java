08839d76a4c699d1e15d24f27bd486b2e882a6fe
unknown <100565368+kaluxxx@users.noreply.github.com>
2024-12-18 20:22:37 +0100

    ajout annotation validation

--- src/main/java/com/webtechlabs/gestionstock/dto/UserDto.java
+++ src/main/java/com/webtechlabs/gestionstock/dto/UserDto.java
@@ -1,13 +1,28 @@
 package com.webtechlabs.gestionstock.dto;
 
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.Set;
 
 @Data
 public class UserDto {
+
     private String uuid;
+
+    @Email(message = "Email should be valid")
+    @NotNull(message = "Email is required")
     private String email;
-    private String password;
+
+    @NotNull(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
+    @Pattern(
+            regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[^a-zA-Z0-9]).+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character"
+    )    private String password;
+
     private Set<String> roles;
 }
