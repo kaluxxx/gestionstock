fde3174350f8ed614fab0ac5e9b24dd04e90e403
kaluxxx <100565368+kaluxxx@users.noreply.github.com>
2024-12-19 15:33:41 +0100

    modification du company repository

--- src/main/java/com/webtechlabs/gestionstock/repository/CompanyRepository.java
+++ src/main/java/com/webtechlabs/gestionstock/repository/CompanyRepository.java
@@ -3,6 +3,8 @@ package com.webtechlabs.gestionstock.repository;
 import com.webtechlabs.gestionstock.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CompanyRepository extends JpaRepository<Company, Long> {
+import java.util.Optional;
 
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findBySiret(String siret);
 }
